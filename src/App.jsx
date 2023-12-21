f9852959b23872a70cde97a402ec62541e7b15ca
marianomayo <marianoagustinmayo@gmail.com>
2023-12-21 18:30:38 -0300

    primer punto

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,16 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import React from 'react';
 import Card from './Component/Card/Card';
 import Product from './Component/Product/Product';
 
 function App() {
- 
-
   return (
     <>
-    <h1>Hola mundo ! </h1>
+      <h1>Hola mundo ! </h1>   
       <Card className="estilo-card" orientation="vertical" size="grande">
-
+        <Product src="https://picsum.photos/300/300" />
       </Card>
-     
-    
     </>
-  )
+  );
 }
 
-export default App
+export default App;
--- src/Component/Card/Card.jsx
+++ src/Component/Card/Card.jsx
@@ -1,13 +1,14 @@
 import React from "react";
 import '../Card/Card.css';
-
+import '../Product/Product.css';
 const Card = ({ className, orientation, size, children }) => {
 
     const cardClasses = `card ${className || ''} ${orientation === 'horizontal' ? 'horizontal' : 'vertical'} ${size === 'grande' ? 'grande' : 'chico'}`;
 
-    console.log(cardClasses)
     return (
-        <div className={cardClasses}>{children}</div>
+        <div className="product-container">
+            <div className={cardClasses}>{children}</div>
+        </div>
     )
 };
 
